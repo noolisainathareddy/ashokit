654cc70b2bc347c86388fcf03f5dfad1429ff875
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2024-02-01 08:11:08 -0600

    added axios lib and enabled sign up optio to create account

---
+++ ashokitAPI/src/main/java/com/training/ashokitAPI/controller/HealthCheck.java
@@ -0,0 +1,2 @@
+package com.training.ashokitAPI.controller;public class HealthCheck {
+}
--- ashokitui/src/signup/SignUp.js
+++ ashokitui/src/signup/SignUp.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import Header from "../header/Header";
 import "../signup/SignUp.css";
+import axios from "axios";
 const SignUp = () => {
     const[info, setInfo] = useState({
-        firstname : "",
-        lastname: "",
-        username : "",
-        emailid: "",
+        firstName : "",
+        lastName: "",
+        userName : "",
+        emailId: "",
         password: "",
-        confirmpassword: ""
+        confirmPassword: ""
     })
     const onSubmitHandler = (e) => {
         e.preventDefault();
         console.log(info);
+        axios.post("http://localhost:8080/addNewUser", info).then((response) => {console.log(response)});
     }
     const onChnageHandler = (e) => {
            setInfo({...info,[e.target.name]:e.target.value}); 
@@ -23,12 +25,12 @@ const SignUp = () => {
             <div class="singup">
                 <div class="signupform">
                     <form onSubmit={onSubmitHandler}>
-                        <input type="text" name="firstname" placeholder="firstname" onChange={onChnageHandler}/><br/><br/>
-                        <input type="text" name="lastname" placeholder="lastname" onChange={onChnageHandler}/><br/><br/>
-                        <input type="text" name="username" placeholder="username" onChange={onChnageHandler}/><br/><br/>
-                        <input type="text" name="emailid" placeholder="emailId" onChange={onChnageHandler}/><br/><br/>
+                        <input type="text" name="firstName" placeholder="First Name" onChange={onChnageHandler}/><br/><br/>
+                        <input type="text" name="lastName" placeholder="Last Name" onChange={onChnageHandler}/><br/><br/>
+                        <input type="text" name="userName" placeholder="User Name" onChange={onChnageHandler}/><br/><br/>
+                        <input type="text" name="emailId" placeholder="Email Id" onChange={onChnageHandler}/><br/><br/>
                         <input type="password" name="password" placeholder="New Password" onChange={onChnageHandler}/><br/><br/>
-                        <input type="password" name="confirmpassword" placeholder="confirm password" onChange={onChnageHandler}/><br/><br/>
+                        <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={onChnageHandler}/><br/><br/>
                         <input type="submit" value="Create Account"/>
                     </form>
                 </div>
