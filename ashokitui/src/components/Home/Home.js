d6e3cbb2bd6f2122cff6fb1512551cc592fc6377
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2024-02-03 14:02:56 -0600

    added carousel to home page

--- ashokitui/src/components/Home/Home.js
+++ ashokitui/src/components/Home/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from "../header/Header";
 import Courses from '../courses/Courses';
 import Footer from '../footer/Footer';
+import Carousel from '../carousel/Carousel';
+import slides from "../../data/CarouselsSlides.json";
 
 const Home = () => {
     return(
         <div>
             <Header/>
+            <Carousel data={slides}/>
             <Courses/>
             <Footer/>
         </div>
